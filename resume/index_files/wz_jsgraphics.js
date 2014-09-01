1a6b1a7a0fe2ff629745e3fb453299c8265687cc
Arnab Shil <skyrudy@bitbucket.com>
2014-09-02 03:44:33 +0600

    resume made

---
+++ resume/index_files/wz_jsgraphics.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /preview/8c4/8c4b7472-3215-11e4-90ae-002590d31986/8c4b7472-3215-11e4-90ae-002590d31986_id_CV_Nicolas_Beauvais_EN_files/wz_jsgraphics.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at www.pdf.investintech.com Port 80</address>
+</body></html>
